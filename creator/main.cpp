582410a17c7a6b5f0d5b603d72ad8412b403880a
Andres Calderon <andresfcalderon@gmail.com>
2016-07-16 17:02:50 -0500

    all IMU data has been shared to the RPi

--- creator/main.cpp
+++ creator/main.cpp
@@ -4,14 +4,14 @@
  * This file is part of MATRIX Creator firmware for MCU
  * Author: Andrés Calderón [andres.calderon@admobilize.com]
  *
- * MATRIX Creator firmware for MCU is free software: you can redistribute 
- * it and/or modify it under the terms of the GNU General Public License 
- * as published by the Free Software Foundation, either version 3 of the 
+ * MATRIX Creator firmware for MCU is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
 
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
 
  * You should have received a copy of the GNU General Public License along
@@ -43,20 +43,40 @@ static msg_t BlinkThread(void *arg) {
   return (0);
 }
 
-static void writeFloatToPSRAM(char *ram, int addr, float data) {
-  char *p = (char *)&data;
-
-  ram[addr] = p[1];
-  ram[addr + 1] = p[0];
-  ram[addr + 2] = p[3];
-  ram[addr + 3] = p[2];  // swapping bytes...
+static void Write4bytesToPSRAM(const char *src, char *ram) {
+  ram[0] = src[1];
+  ram[1] = src[0];
+  ram[2] = src[3];
+  ram[3] = src[2];  // swapping bytes...
 }
+
+static void WriteToPSRAM(const char *src, char *ram, int len) {
+  for (int offset = 0; offset < len; offset = offset + 4) {
+    Write4bytesToPSRAM((const char *)(&src[offset]), (char *)(&ram[offset]));
+  }
+}
+
+struct IMUData {
+  float yaw;
+  float pitch;
+  float roll;
+  float accel_x;
+  float accel_y;
+  float accel_z;
+  float gyro_x;
+  float gyro_y;
+  float gyro_z;
+  float mag_x;
+  float mag_y;
+  float mag_z;
+};
 
 static WORKING_AREA(waIMUThread, 1024);
 static msg_t IMUThread(void *arg) {
   (void)arg;
 
-  float sampleFrequency = 5;// TODO (andres.calderon): Improve the sampleFrequency
+  float sampleFrequency =
+      5;  // TODO (andres.calderon): Improve the sampleFrequency
 
   char *psram = (char *)PSRAM_BASE_ADDRESS;
 
@@ -79,26 +99,37 @@ static msg_t IMUThread(void *arg) {
 
   systime_t time = chTimeNow();  // T0
 
+  IMUData data;
   while (TRUE) {
     time += MS2ST(200);  // Next deadline
 
     chThdSleepMicroseconds(1);
     imu.readGyro();
+    data.gyro_x = imu.calcGyro(imu.gx);
+    data.gyro_y = imu.calcGyro(imu.gy);
+    data.gyro_z = imu.calcGyro(imu.gz);
 
     chThdSleepMicroseconds(1);
     imu.readMag();
+    data.mag_x = imu.calcMag(imu.mx);
+    data.mag_y = imu.calcMag(imu.my);
+    data.mag_z = imu.calcMag(imu.mz);
 
     chThdSleepMicroseconds(1);
     imu.readAccel();
+    data.accel_x = imu.calcMag(imu.ax);
+    data.accel_y = imu.calcMag(imu.ay);
+    data.accel_z = imu.calcMag(imu.az);
 
-    imu_filer_.update(
-        imu.calcGyro(imu.gx), imu.calcGyro(imu.gy), imu.calcGyro(imu.gz),
-        imu.calcAccel(imu.ax), imu.calcAccel(imu.ay), imu.calcAccel(imu.az),
-        imu.calcMag(imu.mx), imu.calcMag(imu.my), imu.calcMag(imu.mz));
+    imu_filer_.update(data.gyro_x, data.gyro_y, data.gyro_z,
+                      data.accel_x, data.accel_y, data.accel_z,
+                      data.mag_x, data.mag_y, data.mag_z);
 
-    writeFloatToPSRAM(psram, 0, imu_filer_.getRoll());
-    writeFloatToPSRAM(psram, 4, imu_filer_.getYaw());
-    writeFloatToPSRAM(psram, 8, imu_filer_.getPitch());
+    data.yaw = imu_filer_.getYaw();
+    data.pitch = imu_filer_.getPitch();
+    data.roll = imu_filer_.getRoll();
+
+    WriteToPSRAM((const char *)&data, psram, sizeof(data));
 
     chThdSleepUntil(time);
   }
@@ -109,7 +140,6 @@ static msg_t IMUThread(void *arg) {
  * Application entry point.
  */
 int main(void) {
-
   halInit();
 
   chSysInit();
