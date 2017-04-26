087a646a7ea7cdad964de514e999c322f98bc15f
yoelrc88 <yoelrc88@gmail.com>
2017-04-26 21:00:10 +0000

    testing

--- creator/main.cpp
+++ creator/main.cpp
@@ -43,6 +43,10 @@ extern "C" {
 #define twoKpDef  (2.0f * 0.75f)  //works with and without mag enabled
 #define twoKiDef  (2.0f * 0.1625f)
 #define betaDef  0.085f
+
+#define TO_RAD(x) (x * 0.0175)  // *pi/180
+#define TO_DEG(x) (x * 57.296)  // *180/pi   
+
 //Used for DCM filter
 const float Kp_ROLLPITCH = 1.2f;  //was .3423
 const float Ki_ROLLPITCH = 0.0234f;
@@ -115,7 +119,7 @@ static msg_t IMUThread(void *arg) {
   imu.begin();
 
   IMUData data;
-  float values[10]; // TODO: Verify the length
+  float values[11]; // TODO: Verify the length
   float ypr[3]; // yaw, pith, roll angles in rad
 
   float last, now,elapsed;      // sample period expressed in milliseconds
@@ -196,11 +200,15 @@ static msg_t IMUThread(void *arg) {
 
 
 
-    // // dcm.G_Dt = elapsed;
-    // // dcm.setSensorVals(values);
-    // // dcm.calDCM();
-    // // dcm.getEulerDeg(values);
-    // // dcm.getEulerRad(values);
+    // dcm.G_Dt = elapsed;
+    // dcm.setSensorVals(values);
+    // dcm.calDCM();
+    // dcm.getEulerDeg(values);
+
+    // values[0] = TO_DEG(values[0]);
+    // values[1] = TO_DEG(values[1]);
+    // values[2] = TO_DEG(values[2]);
+    // dcm.getEulerRad(values);
 
     // // // Organize IMU data for ram writing
     // data.accel_x  = 1; //values[0];
@@ -216,26 +224,28 @@ static msg_t IMUThread(void *arg) {
     // // data.mag_z = values[8];
 
     // // Getting yaw, pitch and roll from DCM output
-    // data.yaw = ypr[0];
-    // data.pitch = ypr[1];
-    // data.roll = ypr[2];
+    // data.yaw = values[0];
+    // data.pitch = values[1];
+    // data.roll = values[2];
 
     // // TODO: just for debug
     // data.mag_x = elapsed;
     // data.mag_y = CH_FREQUENCY;
     // data.mag_z = count++;
 
-    // // data.gyro_x = atan2(data.mag_y, -data.mag_x) * 180.0 / M_PI;
-    // // data.gyro_y = atan2(data.accel_y, data.accel_z) * 180.0 / M_PI;
-    // // data.gyro_z = atan2(-data.accel_x,
-    // //   sqrt(data.accel_y * data.accel_y + data.accel_z * data.accel_z)) * 180.0 / M_PI;
+    // data.gyro_x = atan2(data.mag_y, -data.mag_x) * 180.0 / M_PI;
+    // data.gyro_y = atan2(data.accel_y, data.accel_z) * 180.0 / M_PI;
+    // data.gyro_z = atan2(-data.accel_x,
+    //   sqrt(data.accel_y * data.accel_y + data.accel_z * data.accel_z)) * 180.0 / M_PI;
 
     // psram_copy(mem_offset_imu, (char *)&data, sizeof(data));
 
     if (count++ > 100)
       count = 0;
 
-    chprintf( (BaseChannel *)&SD1, "elapsed: %d \n\r", (int)elapsed); 
+
+    // chprintf( (BaseChannel *)&SD1, "elapsed: %d \n\r", (int)elapsed); 
+    chprintf( (BaseChannel *)&SD1, "%d\r\n", (int)count); 
     chThdSleepMilliseconds(1);
 
     // chThdSleepMilliseconds(20); // TODO : Probably we need this faster
@@ -263,7 +273,7 @@ int main(void) {
   palSetPad(IOPORT3, 17);
   chThdSleepMilliseconds(50);
   palClearPad(IOPORT3, 17);
-  chThdSleepMilliseconds(100);
+  chThdSleepMilliseconds(1000);
 
   /* Configure EBI I/O for psram connection*/
   PIO_Configure(pinPsram, PIO_LISTSIZE(pinPsram));
@@ -272,13 +282,153 @@ int main(void) {
   BOARD_ConfigurePSRAM(SMC);
 
   i2c.Init();
-  /* Creates the imu thread. */
-  chThdCreateStatic(waIMUThread, sizeof(waIMUThread), NORMALPRIO, IMUThread,
-                    NULL);
 
-  /* Creates the hum thread. */
-  chThdCreateStatic(waEnvThread, sizeof(waEnvThread), NORMALPRIO, EnvThread,
-                    NULL);
+  // // Creates the imu thread. 
+  // chThdCreateStatic(waIMUThread, sizeof(waIMUThread), NORMALPRIO, IMUThread,
+  //                   NULL);
+
+  // // Creates the hum thread.
+  // chThdCreateStatic(waEnvThread, sizeof(waEnvThread), NORMALPRIO, EnvThread,
+  //                   NULL);
+
+  /////////////////////// Loop here //////////////////////////////
+  LSM9DS1 imu(&i2c, IMU_MODE_I2C, 0x6A, 0x1C);
+  imu.begin();
+
+  IMUData data;
+  float values[11]; // TODO: Verify the length
+  float ypr[3]; // yaw, pith, roll angles in rad
+
+  float last, now,elapsed;      // sample period expressed in milliseconds
+
+  // DCM Initialization
+  dcm = DCM();
+  
+  // Reading data from the sensors
+  imu.readAccel();
+  imu.readGyro();
+  imu.readMag();
+
+  // organize the sensor data for DCM input
+  values[0] = imu.calcAccel(imu.ax);
+  values[1] = imu.calcAccel(imu.ay);
+  values[2] = imu.calcAccel(imu.az);
+  values[3] = imu.calcGyro(imu.gx);
+  values[4] = imu.calcGyro(imu.gy);
+  values[5] = imu.calcGyro(imu.gz);
+  values[6] = imu.calcMag(imu.mx);
+  values[7] = imu.calcMag(imu.my);
+  values[8] = imu.calcMag(imu.mz);
+
+  // values[9] = maghead.iheading(
+  //   1, 0, 0,
+  //   values[0],
+  //   values[1],
+  //   values[2],
+  //   values[6],
+  //   values[7],
+  //   values[8]);
+
+  dcm.setSensorVals(values);
+  dcm.DCM_init(Kp_ROLLPITCH, Ki_ROLLPITCH, Kp_YAW, Ki_YAW);
+
+  int count = 0;
+  
+  chprintf( (BaseChannel *)&SD1, "** Loop Init \r\n" );  
+  chThdSleepMilliseconds(1);
+
+  while (true) {
+
+  palSetPad(IOPORT3, 17);
+  chThdSleepMilliseconds(1);
+  palClearPad(IOPORT3, 17);
+
+    // // Reading data from the sensors
+    // imu.readAccel();
+    // imu.readGyro();
+    // imu.readMag();
+
+    // // organize the sensor data for DCM input
+    // values[0] = imu.calcAccel(imu.ax);
+    // values[1] = imu.calcAccel(imu.ay);
+    // values[2] = imu.calcAccel(imu.az);
+    // values[3] = imu.calcGyro(imu.gx); 
+    // values[4] = imu.calcGyro(imu.gy);
+    // values[5] = imu.calcGyro(imu.gz);
+    // values[6] = imu.calcMag(imu.mx);
+    // values[7] = imu.calcMag(imu.my);
+    // values[8] = imu.calcMag(imu.mz);
+
+    // // // values[9] = maghead.iheading(
+    // // //   1, 0, 0,
+    // // //   values[0],
+    // // //   values[1],
+    // // //   values[2],
+    // // //   values[6],
+    // // //   values[7],
+    // // //   values[8]);
+
+    // // Sampling time
+
+    // now = chTimeNow();
+    // elapsed = (now - last)/CH_FREQUENCY;
+    // last = now;
+
+
+
+
+    // dcm.G_Dt = elapsed;
+    // dcm.setSensorVals(values);
+    // dcm.calDCM();
+    // dcm.getEulerDeg(values);
+
+    // values[0] = TO_DEG(values[0]);
+    // values[1] = TO_DEG(values[1]);
+    // values[2] = TO_DEG(values[2]);
+    // dcm.getEulerRad(values);
+
+    // // // Organize IMU data for ram writing
+    // data.accel_x  = 1; //values[0];
+    // data.accel_y  = 2; //values[1];
+    // data.accel_z  = 3; //values[2];
+
+    // data.gyro_x = 1; //values[3];
+    // data.gyro_y = 1; //values[4];
+    // data.gyro_z = 1; //values[5];
+
+    // // data.mag_x = values[6];
+    // // data.mag_y = values[7];
+    // // data.mag_z = values[8];
+
+    // Getting yaw, pitch and roll from DCM output
+    data.yaw = 100;
+    data.pitch = 200; 
+    data.roll = 300;
+
+    // // TODO: just for debug
+    // data.mag_x = elapsed;
+    // data.mag_y = CH_FREQUENCY;
+    // data.mag_z = count++; 
+
+    // data.gyro_x = atan2(data.mag_y, -data.mag_x) * 180.0 / M_PI;
+    // data.gyro_y = atan2(data.accel_y, data.accel_z) * 180.0 / M_PI;
+    // data.gyro_z = atan2(-data.accel_x,
+    //   sqrt(data.accel_y * data.accel_y + data.accel_z * data.accel_z)) * 180.0 / M_PI;
+
+    psram_copy(mem_offset_imu, (char *)&data, sizeof(data));
+
+    if (count++ > 1000)
+      count = 0;
+
+
+    // // chprintf( (BaseChannel *)&SD1, "elapsed: %d \n\r", (int)elapsed); 
+    chprintf( (BaseChannel *)&SD1, "%d\r\n", (int)count); 
+    chThdSleepMilliseconds(1);
+
+    // chThdSleepMilliseconds(20); // TODO : Probably we need this faster
+
+  }
+
 
   return (0);
 }
